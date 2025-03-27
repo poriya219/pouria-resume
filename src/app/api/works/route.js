985d16c526d538aeba04df3a5c162f22f838a540
pouria <poriua.219@gmail.com>
2025-03-27 18:19:54 +0330

    add download links

--- src/app/api/works/route.js
+++ src/app/api/works/route.js
@@ -7,7 +7,8 @@ export async function GET(req) {
         "/images/gramatic3.jpg"
       ],
       "cover": "/images/gramatic1.png",
-      "color": "bg-[#84c4eb]"
+      "color": "bg-[#84c4eb]",
+      "link": "https://igtoolspanel.ir/Gramatic.apk"
     },
     {
       "id": "beeaab",
@@ -16,7 +17,8 @@ export async function GET(req) {
         "/images/beeaab3.jpg"
       ],
       "cover": "/images/beeaab1.png",
-      "color": "bg-[#2f3543]"
+      "color": "bg-[#2f3543]",
+      "link": "https://play.google.com/store/apps/details?id=com.health.beeaab&hl=en&pli=1",
     },
     {
       "id": "musicflow",
@@ -24,7 +26,8 @@ export async function GET(req) {
         "/images/music2.jpg"
       ],
       "cover": "/images/music1.png",
-      "color": "bg-[#e6bf62]"
+      "color": "bg-[#e6bf62]",
+      "link": "https://cafebazaar.ir/app/com.poqi.music.MusicFlow",
     },
     {
       "id": "wheater",
@@ -34,7 +37,8 @@ export async function GET(req) {
         "/images/wheater3.png",
       ],
       "cover": "/images/wheater4.png",
-      "color": "bg-[#D97B66]"
+      "color": "bg-[#D97B66]",
+      "link": ""
     }
   ];
   const url = new URL(req.url);
